aca8cfe29dfb77ae83f3b2eeb80b69f9c1c21640
xiaojingming <2413294257@qq.com>
2022-10-19 11:28:10 +0800

    topic type

--- ts/topic2/type.ts
+++ ts/topic2/type.ts
@@ -30,4 +30,47 @@ const isArray = Array.isArray || function f(t) {
   return getType(t) === 'array';
 };
 
+function isPlainObject(target: any) {
+  const { toString, hasOwnProperty } = Object.prototype;
+  if (!target || toString.call(target) !== '[object Object]') {
+    return false;
+  }
+  const proto = Reflect.getPrototypeOf(target);
+  if (!proto) {
+    return true;
+  }
+  const Ctor = hasOwnProperty.call(proto, 'constructor') && proto.constructor;
+  return typeof Ctor === 'function'
+    && toString.toString.call(Ctor) === toString.toString.call(Object);
+}
+
+function P() {}
+const o1 = Object.create(null);
+
+console.log(isPlainObject({})); // true
+console.log(isPlainObject(new (P as any)())); // false
+console.log(isPlainObject(o1)); // true
+
+function isWindow(target: any) {
+  return target !== null && target === target.window;
+}
+
+console.log(isWindow({})); // false
+console.log(isWindow(window)); // true
+
+function isArrayLike(target: any) {
+  const length = !!target && 'length' in target && target.length;
+  const type = getType(target);
+  if (type === 'function' || isWindow(target)) {
+    return false;
+  }
+  return type === 'array'
+    || length === 0
+    || (typeof length === 'number' && length > 0 && (length - 1) in target);
+}
+
+function isElement(target: any) {
+  return !!(target && target.nodeType === 1);
+}
+
 export { };
