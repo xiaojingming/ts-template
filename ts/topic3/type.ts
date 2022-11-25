6d1802e9734b31417f4cbdf8a5dfaa53e5c54217
xiaojingming <2413294257@qq.com>
2022-11-25 18:10:38 +0800

    update:js专题type

--- ts/topic3/type.ts
+++ ts/topic3/type.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-new-object */
 /* eslint-disable no-unused-vars */
 interface O {
   [K: string]: any;
@@ -22,6 +23,50 @@ function getType(target: any): BasicTypes {
 
 function isFunction(target: any) {
   return getType(target) === 'function';
+}
+
+// 纯粹的对象：没有原型的对象，或者构造函数是Object
+function isPlainObject(target: any) {
+  if (!target || typeof target !== 'object') {
+    return false;
+  }
+  const proto = Reflect.getPrototypeOf(target);
+  if (!proto) {
+    return true;
+  }
+  const Ctor = Object.prototype.hasOwnProperty.call(proto, 'constructor') && proto.constructor;
+  return typeof Ctor === 'function'
+    && (() => {}).toString.call(Ctor) === (() => {}).toString.call(Object);
+}
+
+class Person {}
+
+console.log(isPlainObject({})); // true
+console.log(isPlainObject(new Object())); // true
+console.log(isPlainObject(Object.create(null))); // true
+// eslint-disable-next-line prefer-object-spread
+console.log(isPlainObject(Object.assign({ a: 1 }, { b: 2 }))); // true
+console.log(isPlainObject(Object.create({}))); // false
+console.log(isPlainObject(new Person())); // false
+
+function isWindow(target: any) {
+  return target && target.window === target;
+}
+
+// 数组/伪数组
+function isArrayLike(target: any) {
+  const length = !!target && 'length' in target && target.length;
+  const type = getType(target);
+  // 去除function和window类型，这两个类型都包括length属性
+  if (type === 'function' || isWindow(target)) {
+    return false;
+  }
+  return type === 'array' || length === 0
+    || (typeof length === 'number' && length > 0 && (length - 1) in target);
+}
+
+function isElement(target: any) {
+  return target && ((target as HTMLElement).nodeType === 1);
 }
 
 export {};
